d7b81c5584dba88062c9bd841d46875dad1b891c
imxande <alerodriguezlarosa@gmail.com>
2019-12-02 17:55:53 -0500

    added todo data to the app as per instructions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,46 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+
+
+const todo = [
+  {
+      task: 'Workout',
+      id:1,
+      completed: false
+  },
+
+  {
+      task: 'Study Tk',
+      id:2,
+      completed: false
+  },
+
+  {
+      task: 'Study React',
+      id:3,
+      completed: false
+  },
+
+  {
+      task: 'Call my family',
+      id:4,
+      completed: false
+  },
+
+  {
+      task: 'Work on javascript replit challenges',
+      id:5,
+      completed: false
+  },
+
+  {
+      task: 'Read favorite book',
+      id:6,
+      completed: false
+  },
+  
+]
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -8,6 +50,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,25 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+class TodoList extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            
+        };
+    }
+
+    render(){
+        console.log(Todo);
+        return (
+            <div>
+                <h3>My TodoList goes here </h3>
+            </div>
+
+        )     
+        
+    }
+}
+
+export default TodoList;
