9c2221ea47de87035532670149b1d8adbd0bf3b2
imxande <alerodriguezlarosa@gmail.com>
2019-12-02 18:04:17 -0500

    Date function generates id number as per instruction

--- src/App.js
+++ src/App.js
@@ -5,37 +5,37 @@ import TodoList from './components/TodoComponents/TodoList';
 const todo = [
   {
       task: 'Workout',
-      id:1,
+      id:Date.now(),
       completed: false
   },
 
   {
       task: 'Study Tk',
-      id:2,
+      id:Date.now(),
       completed: false
   },
 
   {
       task: 'Study React',
-      id:3,
+      id:Date.now(),
       completed: false
   },
 
   {
       task: 'Call my family',
-      id:4,
+      id:Date.now(),
       completed: false
   },
 
   {
       task: 'Work on javascript replit challenges',
-      id:5,
+      id:Date.now(),
       completed: false
   },
 
   {
       task: 'Read favorite book',
-      id:6,
+      id:Date.now(),
       completed: false
   },
   
@@ -46,7 +46,15 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      todo: todo
+    }
+  }
+
   render() {
+    console.log(todo); // checking if I have my state
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
